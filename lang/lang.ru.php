6926e39062597dd284ee4ff771f6baa7c54f0627
alexbirukov <lord_maloy@mail.ru>
2013-01-12 21:51:41 +0600

    Update russian language
    Update russian language file

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,7 +7,7 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-10-18'
+		'release-date' => '2013-01-12'
 	);
 
 	/**
@@ -15,91 +15,112 @@
 	 */
 	$dictionary = array(
 
-		'Banned IPs' =>
-		'Блокированные IP',
+		'"%s" is not a valid positive integer' => 
+		'"%s" не является положительным числом',
 
-		'IP Colored Lists' =>
-		'Группы IP адресов',
-
-		'Define here when and how IP are blocked' =>
-		'Определите когда и как блокировать IP адреса',
-
-		'IP Address' =>
-		'IP адрес',
-
-		'Failed Count' =>
-		'Количество попыток',
-
-		'Last Attempt' =>
-		'Последняя попытка',
-
-		'User Agent (browser)' =>
-		'User Agent (браузер)',
-
-		'Failures remove successfuly' =>
-		'Список ошибок входа удалён',
-
-		'Error' =>
-		'Ошибка',
-
-		'Date Created' =>
-		'Дата создания',
-
-		'Black list' =>
-		'Чёрный список',
-
-		'Grey list' =>
-		'Серый список',
-
-		'White list' =>
-		'Белый лист',
-
-		'Add' =>
+		'Add' => 
 		'Добавить',
 
-		'IP added successfuly' =>
-		'IP адрес добавлен',
-
-		'Entries remove successfuly' =>
-		'Запись удалена',
-
-		'Enter your email address to be sent a remote unban link with further instructions.' =>
-		'Укажите ваш email для отправки сообщения с ссылкой и инструкцией для дальнейшей разблокировки.',
-
-		'Email sent. Follow the instruction in it.' =>
-		'Письмо отправлено. Следуйте дальнейшим инструкциям.',
-
-		'Unban IP link' =>
-		'Разбанать IP',
-
-		'Please follow this link to unban your IP: ' =>
-		'Пройдите по ссылке для разблокировки вашего IP:',
-
-		'If you do not remember your password, follow the "forgot password" link on the login page.' =>
-		'Если вы забыли свой пароль, нажмите на ссылку "забыли пароль" на странице входа в систему.',
-
-		'Your IP address is currently banned, due to typing too many wrong usernames/passwords.' =>
-		'Ваш текущий IP адрес находится в списке забаненных из-за большого числа попыток входа с неверным логином/паролем.',
-
-		'You can ask your administrator to unlock your account or wait %s minutes.' =>
-		'Вы можете обратиться к администратору для разблокировки вашей учётной записи или подождать %s минут',
-
-		'Alternatively, you can <a href="%s">un-ban your IP by email</a>.' =>
+		'Alternatively, you can <a href="%s">un-ban your IP by email</a>.' => 
 		'Так же вы можете <a href="%s">разблокировать ваш IP по email</a>.',
 
-		'Banned IP address' =>
-		'Забаненные IP адреса',
-
-		'Your IP address is currently <strong>black listed</strong>, due to too many bans.' =>
-		'Ваш IP адрес находится в <strong>чёрном списке</strong> из-за большого числа банов.',
-
-		'Ask your administrator to unlock your IP.' =>
+		'Ask your administrator to unlock your IP.' => 
 		'Обратитесь к администратору для разблокировки вашего IP адреса.',
 
-		'Black listed IP address' =>
+		'Banned IP address' => 
+		'Забаненные IP адреса',
+
+		'Banned IPs' => 
+		'Блокированные IP',
+
+		'Black list' => 
+		'Чёрный список',
+
+		'Black listed IP address' => 
 		'Чёрный список IP адресов',
 
-		'"%s" is not a valid positive integer' =>
-		'"%s" не является положительным числом',
+		'Date Created' => 
+		'Дата создания',
+
+		'Define here when and how IP are blocked' => 
+		'Определите когда и как блокировать IP адреса',
+
+		'Email sent. Follow the instruction in it.' => 
+		'Письмо отправлено. Следуйте дальнейшим инструкциям.',
+
+		'Enter your email address to be sent a remote unban link with further instructions.' => 
+		'Укажите ваш email для отправки сообщения с ссылкой и инструкцией для дальнейшей разблокировки.',
+
+		'Entries remove successfuly' => 
+		'Запись удалена',
+
+		'Error' => 
+		'Ошибка',
+
+		'Failed Count' => 
+		'Количество попыток',
+
+		'Failures remove successfuly' => 
+		'Список ошибок входа удалён',
+
+		'Grey list' => 
+		'Серый список',
+
+		'IP Address' => 
+		'IP адрес',
+
+		'IP added successfuly' => 
+		'IP адрес добавлен',
+
+		'If you do not remember your password, follow the "forgot password" link on the login page.' => 
+		'Если вы забыли свой пароль, нажмите на ссылку "забыли пароль" на странице входа в систему.',
+
+		'Last Attempt' => 
+		'Последняя попытка',
+
+		'Please follow this link to unban your IP: ' => 
+		'Пройдите по ссылке для разблокировки вашего IP:',
+
+		'Unban IP link' => 
+		'Разбанать IP',
+
+		'User Agent (browser)' => 
+		'User Agent (браузер)',
+
+		'White list' => 
+		'Белый список',
+
+		'You can ask your administrator to unlock your account or wait %s minutes.' => 
+		'Вы можете обратиться к администратору для разблокировки вашей учётной записи или подождать %s минут',
+
+		'Your IP address is currently <strong>black listed</strong>, due to too many bans.' => 
+		'Ваш IP адрес находится в <strong>чёрном списке</strong> из-за большого числа банов.',
+
+		'Your IP address is currently banned, due to typing too many wrong usernames/passwords.' => 
+		'Ваш текущий IP адрес находится в списке забаненных из-за большого числа попыток входа с неверным логином/паролем.',
+		
+		'Black Listed IPs' => 
+		'Чёрный список IP',
+
+		'Grey Listed IPs' => 
+		'Серый список IP',
+
+		'White Listed IPs' => 
+		'Белый список IP',
+
+		'There are currently' => 
+		'В настоящее время',
+
+		'%d blocked IPs.' => 
+		'%d заблокированных IP.',
+
+		'Unban via email' => 
+		'Разблокировать по электронной почте',
+
+		'%s is not installed properly and won\'t work until this is fixed. Ensure latest version is intalled.' => 
+		'%s не установлен должным образом и не может работать корректно. Убедитесь, что установлена последняя версия.',
+
+		'An error occured' => 
+		'Произошла ошибка',
 
 	);
